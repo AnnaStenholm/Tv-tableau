3ed18e199a4b7ebbe3b07eacaa87647a3edb8bee
Anna Stenholm <annastenholm@Annas-MacBook-Air.local>
2021-02-22 12:51:43 +0100

    new changes

--- data/script.js
+++ data/script.js
@@ -1,4 +1,5 @@
 
+
 /*Side-Menu*/
 
 
@@ -10,145 +11,76 @@ function toggleMenu() {
   icon.classList.toggle('fa-times');
 }
 
-
-
-
-
-/*
-newUl.classList.contains('.ul-menu');
-const newLi = document.createElement('li');
-newUl.appendChild('newLi');
-const textNode = document.createTextNode('SVT 1')
-newLi.appendChild('textNode');*/
-
-
-/*hämta program från kanal*/
-
-
+/* Ladda titel på vald kanal*/
 
 window.addEventListener("load", () => {
-  loadData()
+  setChannel("SVT2");
+
 });
 
 
-let programData = []; //spara ner i array.
-
-function loadData() {
-  fetch("/data/SVT2.json")
+function setChannel(chanel) {
+  document.querySelector("#js-loading").classList.remove("hidden");
+  fetch("/data/" + chanel + ".json")
     .then(res => res.json())
     .then((data) => {
       console.log(data)
       programData = data;
-      render()
+      render(data);
+      document.querySelector("#js-loading").classList.add("hidden");
     });
+  document.getElementById("js-title").innerHTML = chanel;
 }
-
-
-
-var timeStart=start.slice(11,16);
-
 
 /* Map-funktion*/
-function render() {
-  let html = programData
-    .map((dataPoint) => {
-      return` 
-      <li class="list-group-item">
-                  <div class="tid"><strong>${moment(dataPoint.start).format('LT')} </strong></div>
- <div class="namn">${dataPoint.name} </div></li>`;
- 
-    })
+function render(data) {
+  tidSorterare(data);
+  if (!previousItems) {
+    data = filterTid(data);
+  }
+  let html = `<ul class="list-group list-group-flush">`
+  if (!previousItems) {
+    html += `<li class="list-group-item show-previous" "onclick"=showRecent()>Visa tidigare program</li>`
+  }
+
+
+  html += data.map((programData) => {
+    return `<li class="list-group-item">
+                  <strong>${moment(programData.start).format('HH:mm')} </strong>
+ <div>${programData.name} </div></li>`;
+
+  })
     .join("");
-    document.getElementById("js-schedule").innerHTML = html;
-        
-}
-render();
+  html += '</ul>';
+  document.getElementById("js-schedule").innerHTML = html;
 
-
-function setChannel(chanel){
-  let chanel = "./data/"+chanel+".json";
-  fetch(chanel)
-  .then(res => res.json())
-  .then((data) => {
-    console.log(data)
-    let kanalInfo = programData
-    .map((dataPoint) => {
-    return `        
-      <ul class="list-group list-group-flush">
-        <li  class="list-group-item show-previous" onclick="setChannel2('${dataPoint.chanel}')">Visa tidigre program</li>`;
-
-      })
-{
-var name=data[i].name;
-var start=data[i].start;
-
-var startTime=start.slice(11,16);
-
-  
-} 
-document.getElementById("js-title").innerHTML = chanel;
-  
-    
-  });
 }
 
-
-
-
-
-
-
- /*Sortera på tid */
+/*Sortera på tid */
 function tidSorterare() {
-programData.sort((a,b) =>{
-  if (a.start > "2021-02-10T15:00:00+01:00") {
-    return 1;
-  }
-  if (b.start < a.start) {
-    return -1;
-  }
-  return 0;
-});
-render();
+
+  data.sort((a, b) => {
+    if (a.start < b.start) {
+      return -1;
+    }
+    if (a.start > b.start) {
+      return 1;
+    }
+    return 0;
+  });
 }
 
+let previousItems = false;
+let data = [];
+function showRecent() {
+  previousItems = true;
+  render(programData);
 
-
-/* Filtrera bort program innan 15:00 */
-const filtered = programData.filter(function (p) {
-  if (p.start > "2021-02-10T15:00:00+01:00") {
-    return true;
+  function filterTid(data) {
+    return data.filter(function (program) {
+      const startTime = new Date(program.start);
+      const currentTime = new Date();
+      return startTime.getHours() > currentTime.getHours();
+    })
   }
-
-});
-
-console.log(filtered);
-
-
-
-
-
-
-/*
-    function setChannel1(){
-        fetch('/data/SVT1.json')
-  .then(
-    function(response) {
-      if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-          response.status);
-        return;
-      }
-
-      // Examine the text in the response
-      response.json().then(function(data)
-      => { program = data.value{
-        console.log(data);
-      });
-    }
-  )
-  .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-  });
-    }
-    */
+}
